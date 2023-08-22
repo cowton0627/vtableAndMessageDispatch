7ed5d27c224aa9e1f8d022d0dae3d2992a3fb5f2
Charles Cheng <charles_cheng@microjet.com.tw>
2023-08-22 14:45:00 +0800

    vtable. witness table. message dispatch

--- vtableAndMessageDispatch/ViewController.swift
+++ vtableAndMessageDispatch/ViewController.swift
@@ -11,9 +11,70 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let base: Showable = Base()
+        base.show()  // 輸出：This is base protocol.
+
+        let derived: Showable = Derived()
+        derived.show()         // 輸出：This is base protocol.
+//        if let someDerived = derived as? Derived {
+//            someDerived.show() // 輸出：This is derived class.
+//            someDerived.fun()
+//        }
+        print()
+        
+        // 替換方法
+        let method = class_getInstanceMethod(MyClass.self, #selector(MyClass.originalMethod))
+//        let originalImplementation = method_getImplementation(method!)
+        
+        let myObject = MyClass()
+        myObject.originalMethod()  // Prints: "This is the original method"
+        
+//        let newImplementationBlock: @convention(block) (AnyObject) -> Void = { _ in
+//            print("This is the replacement method")
+//        }
+        
+        let newImplementationBlock: @convention(block) (String) -> Void = { (stringValue) in
+            print("This is the replacement method\(stringValue)")
+        }
+        let newImplementation = imp_implementationWithBlock(newImplementationBlock)
+
+        method_setImplementation(method!, newImplementation)
+        
+        myObject.originalMethod()  // Prints: "This is the replacement method"
     }
 
 
+}
+
+protocol Showable {
+    func show()
+}
+
+extension Showable {
+    func show() {
+        print("This is base protocol.")
+    }
+}
+
+class Base: Showable {
+//    func show() {
+//        print("This is base protocol.")
+//    }
+}
+
+class Derived: Base {
+    func show() {
+        print("This is derived class.")
+    }
+    func fun() {
+        print("123")
+    }
+    
+}
+
+class MyClass: NSObject {
+    @objc dynamic func originalMethod() {
+        print("This is the original method")
+    }
 }
 
